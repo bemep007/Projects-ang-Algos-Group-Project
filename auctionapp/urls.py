09414738f84c5cc6024dcf2200b50538298d4161
bemep007 <diana.brooks@hotmail.com>
2021-07-24 16:30:19 -0500

    updates views.py, models.py, urls.py

--- auctionapp/urls.py
+++ auctionapp/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+
+
+    #Account page urls:
+    # path('my_account/<int:user_id>', views.my_account),
+    # path('my_account/<int:user_id>/update', views.update_account),
 ]
